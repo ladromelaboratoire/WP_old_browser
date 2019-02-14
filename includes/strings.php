70488e43b136939e22b4c384360defe5fa9c3291
La Drôme Laboratoire <45164229+ladromelaboratoire@users.noreply.github.com>
2019-02-14 10:01:25 +0100

    Support multi string replace in templates

---
+++ includes/strings.php
@@ -0,0 +1,21 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/*****************
+*	Array for template strings
+*	v 1.0.0
+*	Fevrier 2019
+*	Author La Drome Laboratoire
+*
+*
+******************/
+
+$strings["pattern"][] = "/site_title/";
+$strings["pattern"][] = "/site_url/";
+$strings["pattern"][] = "/logo_url/";
+
+$strings["repl"][] = get_bloginfo('name');
+$strings["repl"][] = get_bloginfo('url');
+$strings["repl"][] = old_browser_get_logouri();
+
+?>
